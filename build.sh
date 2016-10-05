a795a6fc604f4f7eaafbbc77f43407dcff3eb925
sneJ p <sneJ-@users.noreply.github.com>
2016-10-05 17:12:38 +0200

    Update build.sh
    changed for use with ubuntu 16.04 x64

--- build.sh
+++ build.sh
@@ -1,24 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-if ["$1" == "-i"]
+if [ $1 == "-i" ]
 then
 	#install main ALE dependencies
-	apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake -y
+	sudo add-apt-repository universe 
+	sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake python-pip -y
 
 	#install dqn dependencies
-	pip install numpy petname imageio
+	sudo -H pip install petname imageio
 	export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc0-cp27-none-linux_x86_64.whl
-	pip install --upgrade $TF_BINARY_URL
+	sudo -H pip install --upgrade $TF_BINARY_URL
 fi
 
 #build ALE
 mkdir build && cd build
-cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF -DBUILD_EXAMPLES=ON ..
-make -j 4
+sudo cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF -DBUILD_EXAMPLES=ON ..
+sudo make -j 4
 
 #set up python interface
 cd ..
-pip install .
+sudo -H pip install .
 
 #compile Java agents
 cd doc/java-agent/code
