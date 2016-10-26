7ced1c08db20bfe3c408d4b0f81e4999c08c0162
sneJ p <sneJ@trash-mail.com>
2016-10-27 00:16:06 +0200

    ant dependency added, bugifx in build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ $1 == "-i" ]
 then
 	#install main ALE dependencies
 	sudo add-apt-repository universe 
-	sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake python-pip -y
+	sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake python-pip ant -y
 
 	#install dqn dependencies
 	sudo -H pip install petname imageio
@@ -20,7 +20,7 @@ sudo make -j 4
 
 #set up python interface
 cd ..
-sudo -H pip --upgrade --force-reinstall install .
+sudo -H pip install --upgrade --force-reinstall .
 
 #compile Java agents
 cd doc/java-agent/code
